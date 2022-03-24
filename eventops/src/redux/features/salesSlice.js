0fd8924f305d006527c5224376dc7044241422af
mrff88 <mrff88@gmail.com>
2022-03-23 23:24:01 -0500

    add async action to sale slice

--- eventops/src/redux/features/salesSlice.js
+++ eventops/src/redux/features/salesSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getEpaycoSale } from '../../api/sales';
+
+export const getEpaycoSaleAsync = createAsyncThunk(
+  'sales/getEpaycoSale',
+  async (id, { rejectWithValue }) => {
+    try {
+      const response = await getEpaycoSale(id);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 export const salesSlice = createSlice({
   name: 'sales',
@@ -10,8 +23,27 @@ export const salesSlice = createSlice({
       state.sales.push({ ...newSale });
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getEpaycoSaleAsync.pending, (state) => {
+        state.isSearching = true;
+      })
+      .addCase(getEpaycoSaleAsync.fulfilled, (state, { payload }) => {
+        state.isSearching = false;
+        state.ePaycoSale = payload;
+      })
+      .addCase(getEpaycoSaleAsync.rejected, (state, { payload }) => {
+        state.isSearching = false;
+        state.ePaycoError = payload;
+      });
+  },
 });
 
 export const { addNewSale } = salesSlice.actions;
+
+// selector
+export const selectIsSearching = (state) => state.sales.isSearching;
+export const selectePaycoSale = (state) => state.sales.ePaycoSale;
+export const selectePaycoError = (state) => state.sales.ePaycoError;
 
 export default salesSlice.reducer;
